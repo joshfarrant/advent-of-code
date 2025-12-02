99352ad8959bc5c5e03acd27316039f5b4187919
Josh Farrant <joshfarrant@github.com>
2025-12-02 11:16:28 +0000

    refactoring

--- src/2025/02/solutions/02/solution.ts
+++ src/2025/02/solutions/02/solution.ts
@@ -1,46 +1,30 @@
 const parseInput = (str: string) =>
   str.split(',').map(x => x.split('-')) as [string, string][];
 
-const chunk = (str: string, size: number) =>
-  str.match(new RegExp('.{1,' + size + '}', 'g')) as string[];
+let regexMap = new Map<number, RegExp>();
 
-const allEqual = (arr: unknown[]) => arr.every(x => x === arr[0]);
-
-const sum = (a: number, b: number) => a + b;
-
-const factorMap = new Map<number, number[]>();
-const getFactors = (num: number): number[] => {
-  if (factorMap.has(num)) return factorMap.get(num)!;
-
-  const factors = [];
-
-  for (let i = 1; i <= num / 2; i++) {
-    if (num % i === 0) {
-      factors.push(i);
-    }
+const isRepeatedString = (str: string, size: number) => {
+  let regex = regexMap.get(size);
+  if (!regex) {
+    regex = new RegExp('.{1,' + size + '}', 'g');
+    regexMap.set(size, regex);
   }
 
-  factorMap.set(num, factors);
-
-  return factors;
+  const chunks = str.match(regex) as string[];
+  return chunks.every(x => x === chunks[0]);
 };
 
 const isInvalidId = (id: string) => {
-  const factors = getFactors(id.length);
-
-  return factors.some(factor => {
-    const chunks = chunk(id, factor);
-    return allEqual(chunks);
-  });
+  for (let factor = 1; factor <= id.length / 2; factor++) {
+    if (id.length % factor === 0 && isRepeatedString(id, factor)) return true;
+  }
 };
 
 const sumInvalidIds = (start: string, end: string) => {
   let total = 0;
 
-  const startNum = Number(start);
-  const endNum = Number(end);
-
-  for (let id = startNum; id <= endNum; id++) {
+  let id = Number(start);
+  while (id <= Number(end)) {
     if (isInvalidId(String(id))) {
       total += id;
     }
